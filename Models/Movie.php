86fb0fc29e240fa38aa5ef0f50218cc86881fe9e
israr <misrar09@gmail.com>
2024-01-15 07:58:55 +0100

    added construct and method

--- Models/Movie.php
+++ Models/Movie.php
@@ -10,4 +10,23 @@ class Movie
     public $language;
     public $cast;
     public $rating;
+
+
+
+
+    public function __construct($name, $year, $genere, $language, $cast, $rating)
+    {
+        $this->name = $name;
+        $this->year = $year;
+        $this->genere = $genere;
+        $this->language = $language;
+        $this->cast = $cast;
+        $this->rating = $rating;
+    }
+
+    public function getMovie()
+    {
+        $result = $this->name . "-" . $this->year . "-" . $this->genere . "-" . $this->language . "-" . $this->cast . "-" . $this->rating;
+        return $result;
+    }
 }
