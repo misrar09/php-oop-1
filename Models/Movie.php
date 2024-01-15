66e8a19956fe3f0a6929663b9cac7cbad5d63a07
israr <misrar09@gmail.com>
2024-01-15 07:42:14 +0100

    first commit

---
+++ Models/Movie.php
@@ -0,0 +1,13 @@
+<!-- Creating a movie class -->
+
+<?php
+
+class Movie
+{
+    public $name;
+    public $year;
+    public $genere;
+    public $language;
+    public $cast;
+    public $rating;
+}
